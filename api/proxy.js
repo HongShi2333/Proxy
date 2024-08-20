0201adad4e527c68e3ceffa9a40b2642e9c6f5ee
HongShi2333 <1986977075@qq.com>
2024-08-20 18:01:01 +0800

    dev

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://laplace.live/stats/477792";//your website url
+  let target = "https://laplace.live/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
